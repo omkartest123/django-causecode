6f80113b6fbfb5c01df75fcb841c566ce47fa520
omkartest123 <31002596+omkartest123@users.noreply.github.com>
2018-01-04 16:04:02 +0530

    Update urls.py

--- causecode/causecode/urls.py
+++ causecode/causecode/urls.py
@@ -1,4 +1,5 @@
 """causecode URL Configuration
+lets see
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
